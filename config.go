3ce4b6b6fb3321f62bd2b7decddb928707880e85
dreamer2q <dreamer22qq@gmail.com>
2020-12-30 00:49:19 +0800

    add comment

--- config.go
+++ config.go
@@ -1,7 +1,7 @@
 package main
 
 type Config struct {
-	Addr      string
+	Addr      string //本地监听地址
 	Token     string //语雀 Token
 	Namespace string //语雀 Namespace
 }
@@ -9,6 +9,7 @@ type Config struct {
 var (
 	config = &Config{
 		Addr:      ":8080",
+		// 创建Token的时候不要选择write权限，仅read即可
 		Token:     "8yx2GugoI9kLsKAQ9pDR1f2S371Vwh4D8EHzx1OW",
 		Namespace: "dreamer2q/blog",
 	}
